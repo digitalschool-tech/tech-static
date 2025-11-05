cf42503c41638b9b71f9e29adb0822fdac49f396
Rigon Ramajli <rigon.ramajli@starlabspro.com>
2025-11-05 11:29:32 +0100

    feat: redesign

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
+import { Inter } from 'next/font/google'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Digital School - R&D',
+  description: 'Digital School - R&D',
+  generator: 'Digital School - R&D',
 }
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 
 export default function RootLayout({
   children,
@@ -16,16 +17,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <style>{`
-html {
-  font-family: ${GeistSans.style.fontFamily};
-  --font-sans: ${GeistSans.variable};
-  --font-mono: ${GeistMono.variable};
-}
-        `}</style>
-      </head>
-      <body>{children}</body>
+      <head />
+      <body className={`${inter.variable} font-sans text-gray-900`}>{children}</body>
     </html>
   )
 }
